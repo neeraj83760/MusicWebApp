24dcee97002214a16302abf905dfc49f27092935
neeraj <neerajkumarghildiyal@gmail.com>
2023-08-16 17:06:04 +0530

    Done with forward and backword button functionality in JS

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+const image = document.querySelector('img')
+const title = document.getElementById('title')
+const artist = document.getElementById('artist')
+
 const music = document.querySelector('audio')
 const prevBtn = document.getElementById('prev')
 const playBtn = document.getElementById('play')
 const nextBtn = document.getElementById('next')
 
+
+// Music 
+
+const songs = [
+
+    {
+        name:'jacinto-1',
+        displayName:'Electric Chill Machine',
+        artist:'Jacinto Design',
+    },
+    {
+        name:'jacinto-2',
+        displayName:'Seven Nation Army (Remix)',
+        artist:'Jacinto Design',
+    },
+    {
+        name:'jacinto-3',
+        displayName:'Goodnight, Disco Queen',
+        artist:'Jacinto Design',
+    },
+    {
+        name:'metric-1',
+        displayName:'FrontRow(Remix)',
+        artist:'Metric/Jacinto Design',
+    }
+]
+
 // Check if playing 
 
 let isPlaying = false;
-
-
 
 // Play
 
@@ -30,5 +59,62 @@ function pauseSong(){
 // Play and Pause Event listener
 
 playBtn.addEventListener('click', ()=> (isPlaying ? pauseSong() : playSong()))
+
+
+// Update DOM
+
+function loadSong(song){
+
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+
+
+// Current Song
+let songIndex = 0;
+
+// Prev song function 
+
+function prevSong(){
+
+    songIndex--;
+    if(songIndex < 0){
+
+    songIndex = songs.length - 1; 
+    }
+    // console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+
+}
+
+// Next song function 
+
+function nextSong(){
+
+    songIndex++;
+    if(songIndex > songs.length - 1){
+
+        songIndex = 0;
+    }
+    // console.log(songIndex);
+    loadSong(songs[songIndex]);
+    playSong();
+
+}
+
+
+// Onload select first song
+
+loadSong(songs[songIndex]);
+
+// Event Listeners 
+
+prevBtn.addEventListener('click',prevSong)
+nextBtn.addEventListener('click',nextSong)
+
+
 
 
